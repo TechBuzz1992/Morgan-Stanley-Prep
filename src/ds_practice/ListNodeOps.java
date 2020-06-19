91634600915a7df9e42f64ac778b082d86d5f858
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-19 19:29:48 +0530

    commit

--- src/ds_practice/ListNodeOps.java
+++ src/ds_practice/ListNodeOps.java
@@ -6,18 +6,25 @@ public class ListNodeOps {
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(-8);
+        head.next.next.next = new ListNode(2);
 
-        new ListNodeOps().linkedListTraversal(head);
+        ListNodeOps obj = new ListNodeOps();
 
-        
+        obj.linkedListTraversal(head);
+
+        int ans = obj.getNthFromEnd(head, 3).data;
+
+        System.out.println(ans);
+
+        System.out.println("Get the largest value : " + obj.getTheLargestNodeValue(head));
+
     }
 
-    public void linkedListTraversal(ListNode head){
+    public void linkedListTraversal(ListNode head) {
         ListNode dummy = head;
-        while(dummy!=null){
+        while (dummy != null) {
             System.out.print(dummy.data + " ");
             dummy = dummy.next;
         }
@@ -25,5 +32,34 @@ public class ListNodeOps {
         System.out.println("\nHead data is : " + head.data);
 
     }
-    
+
+    public ListNode getNthFromEnd(ListNode head, int n) {
+        ListNode dummy = head;
+        ListNode ans = head;
+        int count = 1;
+        while (dummy != null) {
+            dummy = dummy.next;
+            count++;
+            if (count > n+1) {
+                ans = ans.next;
+
+            }
+
+        }
+        return ans;
+
+    }
+
+    public int getTheLargestNodeValue(ListNode head){
+        int ans = Integer.MIN_VALUE;
+        ListNode dummy = head;
+        while(dummy!=null){
+            if(ans<dummy.data){
+                ans = dummy.data;
+            }
+            dummy = dummy.next;
+        }
+        return ans;
+    }
+
 }
---
+++ src/ds_practice/SortingKeysMap.java
@@ -0,0 +1,42 @@
+package ds_practice;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Person {
+    private int id;
+    private String name;
+
+    Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+public class SortingKeysMap {
+    public static void main(String[] args) {
+
+    }
+
+    // public Set<Person> sortMapKeys(Set<Person> set) {
+
+    // }
+
+}
