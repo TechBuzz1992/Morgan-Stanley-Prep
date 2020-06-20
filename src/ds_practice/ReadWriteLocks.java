3ff5acece11d23f870ba9641161449f5de6cc6bc
TechBuzz1992 <techbuzz1992@outlook.com>
2020-06-20 21:55:07 +0530

    commit

--- src/ds_practice/ReadWriteLocks.java
+++ src/ds_practice/ReadWriteLocks.java
@@ -1,12 +1,53 @@
 package ds_practice;
 
 import java.util.*;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class Foo {
+    private String name;
+
+    public Foo(String name) {
+        this.name = name;
+    }
+
+    public void setName(String s) {
+        this.name = s;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
+
+class Example {
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private Foo foo = new Foo("harsh");
+
+    String getFooString() {
+        lock.readLock().lock();
+        String name;
+        try {
+            name = foo.getName();
+        } finally {
+            lock.readLock().unlock();
+        }
+        return name;
+    }
+
+    void setName() {
+        lock.writeLock().lock();
+        try {
+            foo.setName("xyz");
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+}
 
 public class ReadWriteLocks {
     public static void main(String[] args) {
-        
+
     }
 
-    
-    
 }
